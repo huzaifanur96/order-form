c956c6a287914dcb6fac483a6c4fa6a1fe26d274
Akash Kumar Sharma <vyom@vyomnet.tech>
2020-11-10 18:09:34 +0530

    form rendering working

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +14,24 @@ class OrderController extends AbstractController
     /**
      * @Route("/", name="order")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/OrderController.php',
+        // create the form
+        $order = new Order;
+        $form = $this->createForm(OrderType::class, $order);
+
+        // handle form request
+        $form->handleRequest($request);
+
+        // check form submission
+        if ($form->isSubmitted()) {return $this->render('order_form/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+            // TODO
+        }
+
+        return $this->render('order_form/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Order;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,27 @@ class OrderType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('funeralHome')
-            ->add('contact')
-            ->add('funeralHomeDate')
-            ->add('funeralHomeStreet')
-            ->add('funeralHomeCity')
-            ->add('funeralHomeState')
-            ->add('funeralHomeZip')
-            ->add('funeralHomeTelephone')
-            ->add('funeralHomeEmail')
-            ->add('funeralHomeShipMerchandiseTo')
+            ->add('funeralHome', TextType::class)
+            ->add('contact', TextType::class)
+            ->add('funeralHomeDate', DateType::class)
+            ->add('funeralHomeStreet', TextType::class)
+            ->add('funeralHomeCity', TextType::class)
+            ->add('funeralHomeState', ChoiceType::class, [
+                'choices' => [
+                    'State 1' => 'State 1',
+                    'State 2' => 'State 2',
+                    'State 3' => 'State 3',
+                ],
+            ])
+            ->add('funeralHomeZip', TextType::class)
+            ->add('funeralHomeTelephone', TextType::class)
+            ->add('funeralHomeEmail', EmailType::class)
+            ->add('funeralHomeShipMerchandiseTo', ChoiceType::class, [
+                'choices' => [
+                    'Funeral Director' => 'Funeral Director',
+                    'Family of Deceased' => 'Family of Deceased',
+                ],
+            ])
         ;
     }
 
